bc039cd5a3a352058347e89383c1c41033be57db
PavanCodes05 <pavancodes05@gmail.com>
2024-07-04 20:30:08 +0530

    Day-9 Project Completed

---
+++ TheSecretAuction.py
@@ -0,0 +1,32 @@
+print("Welcome to the secret auction! ")
+
+bids = {}
+
+def auction(name, bid):
+    bids[name] = bid    
+
+
+bid = True
+
+while bid:
+    name = input("Enter Your Name: ")
+    bid = int(input("Enter Your Bid: $"))
+
+    auction(name, bid)
+
+
+    user = input("Do you wanna place a bid?, Yes or No: \n")
+    
+    if user.lower() == 'no':
+        bid = False
+        
+        highest_bid = 0
+        highest_bidder = ""
+        
+        for bid in bids:
+            if bids[bid] > highest_bid:
+                highest_bid = bids[bid]
+                highest_bidder = bid
+        
+        print(f"\n{highest_bidder} Wins the auction by bidding ${highest_bid}")
+        break
